fcba485c3c6ba7ce481082f2f4af09f7abae6e93
IceHawkey95 <nojus.cininas@stud.mif.vu.lt>
2024-09-19 14:38:10 +0300

    Pridetas experimentinis page'as pasizaidimui, kadangi cia vps pieva man

--- brainfreeze/src/Header.tsx
+++ brainfreeze/src/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import About from "./pages/About";
+import Game from "./pages/Game";
 
 function Header() {
     const [currentPage, setCurrentPage] = useState('home');
@@ -7,16 +8,19 @@ function Header() {
     // Handler functions to switch between pages
     const goToHome = () => setCurrentPage('home');
     const goToAbout = () => setCurrentPage('about');
+    const goToGame = () => setCurrentPage('game');
     return(
         <header>
             <h2>BrainFreeze</h2>
             <nav>
                 <button onClick={goToHome}>Home</button>
                 <button onClick={goToAbout}>About</button>
+                <button onClick={goToGame}>Game</button>
             </nav>
 
             <main>
                 {currentPage === 'about' && <About></About>}
+                {currentPage === 'game' && <Game></Game>}
             </main>
         </header>
     );

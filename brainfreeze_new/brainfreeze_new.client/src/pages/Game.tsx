439fc1aa6b8d0bd99b5ec8b2da1640180f8527dc
pagn4 <agne.petke@gmail.com>
2024-10-16 11:58:54 +0300

    created some front-end for the card flip game

--- brainfreeze_new/brainfreeze_new.client/src/pages/Game.tsx
+++ brainfreeze_new/brainfreeze_new.client/src/pages/Game.tsx
@@ -1,15 +1,49 @@
 import './Game.css'
+import bf from '../assets/bf.png';
+import gb from '../assets/gb.png';
+import ic from '../assets/icecube.png';
+import th from '../assets/think.png';
+import sk from '../assets/skate.png';
+import zz from '../assets/sleep.png';
+import bb from '../assets/bigbrain.png';
+import gr from '../assets/gear.png';
+import cr from '../assets/icecream.png';
+import { useState } from 'react';
 
-function Game() {
+const images = [
+  bf, bf, gb,
+  gb, ic, ic,
+  th, th, sk,
+  sk, zz, zz,
+  bb, bb, gr,
+  gr, cr, cr
+];
+
+const Game: React.FC = () => {
+  const [visibleImages, setVisibleImages] = useState<boolean[]>(
+    new Array(images.length).fill(false)
+  );
+
+  const handleClick = (index: number) => {
+    const newVisibleImages = [...visibleImages];
+    newVisibleImages[index] = true; 
+    setVisibleImages(newVisibleImages);
+  };
 
   return (
-    <>
-      <p className="experiment-text">
-        This is a minimalistic experimental page <br></br>
-        It's really basic just to get used to the program
-      </p>
-    </>
-  )
-}
+    <div className="grid-container">
+      {images.map((imageUrl, index) => (
+        <div key={index} className="grid-item" onClick={() => handleClick(index)}>
+          {visibleImages[index] ? (
+            <img src={imageUrl} alt={`Image ${index + 1}`} className="grid-image" />
+          ) : (
+            <div className="placeholder"></div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 export default Game

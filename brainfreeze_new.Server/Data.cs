f2df522c8491f7cffad4daaee884b16664d9c782
Nojus <nojus.cininas@mif.stud.vu.lt>
2024-10-24 11:55:52 +0300

    Added 10 req + fixed naming conventions and added linq + setter getter

--- brainfreeze_new.Server/Data.cs
+++ brainfreeze_new.Server/Data.cs
@@ -3,10 +3,26 @@ using System.Reflection.Metadata.Ecma335;
 namespace brainfreeze_new.Server
 {
     public class Data
-    { 
-        public List<object> createdList { get; set; } = new List<object>();
-        public int level => createdList.Count;
-        public List<object> expectedList { get; set; } = new List<object>();
+    {
+
+        private List<object> _createdList = [];     // Let's use _ for private fields:)
+        
+        public List<object> CreatedList
+        {
+            get
+            {
+                return _createdList;
+            }
+            set
+            {
+                _createdList = value;
+            }
+        }
+
+        // Also possible to rewrite it like this:
+        // public List<object> createdList { get; set; } = new List<object>();     
+        public int Level => CreatedList.Count;
+        public List<object> ExpectedList { get; set; } = [];
 
     }
 

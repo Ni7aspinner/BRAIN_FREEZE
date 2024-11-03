fb79d9cfd59003fedd644e570701a92fb34dd990
Nikita Savickis <nikita.savickis@mif.stud.vu.lt>
2024-11-03 21:49:55 +0200

    added functionality

--- brainfreeze_new.client/src/pages/NRG.tsx
+++ brainfreeze_new.client/src/pages/NRG.tsx
@@ -1,8 +1,18 @@
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 import './NRG.css';
 import Grid from '../assets/Grid-1000-10-2-100.png';
+interface Data {
+  createdList: number[];
+  level: number;
+  expectedList: number[]; 
+  difficulty: 'VeryEasy' | 'Easy' | 'Medium' | 'Hard' | 'Nightmare' | 'Impossible';
+}
 
 function NRG() {
+  const [datas, setData] = useState<Data>();
+  const [dataString1, setDataString1] = useState<string>(''); 
+  const [dataString2, setDataString2] = useState<string>(''); 
+
     const [flashingButtons, setFlashingButtons] = useState(Array(25).fill(false));
     const buttonPositions = [
         { top: '0.2%', left: '0%' },
@@ -34,22 +44,82 @@ function NRG() {
         { top: '79.3%', left: '40%' },
         { top: '79.3%', left: '60%' },
         { top: '79.3%', left: '80%' },
-  ];  
-  const handleFlash = (index: number) => {
-    setFlashingButtons((prev) => {
-        const newState = [...prev];
-        newState[index] = true;
-        return newState;
+  ];
+
+  useEffect(() => {
+    populateData();
+  }, []);
+  useEffect(() => {
+    handleArray();
+  }, [datas?.createdList]);
+  const populateData = async () => {
+    try {
+        console.log('Populating data');
+        const response = await fetch(`https://localhost:7005/api/NRG`); 
+        if (!response.ok) {                                               
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const results = await response.json();
+        const data: Data = results.data;
+        console.log(results.message);
+        setData(data);
+        const dataString1 = data.createdList.join(', ');
+        setDataString1(dataString1);
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+    }
+  };
+
+  async function postData(data : Data) 
+  {
+    try {
+      console.log('Posting data:', JSON.stringify(data));
+      const response = await fetch('https://localhost:5219/api/NRG', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
       });
+
+      if (response.ok) {
+      const results = await response.json();
+      const result = results.data;
+      console.log(results.message);
+      console.log('API response: ', result);
+      setData(result);
+      const dataString1 = result.createdList.join(', '); 
+      setDataString1(dataString1); 
+      const dataString2 = result.expectedList.join(', ');
+      setDataString2(dataString2);
+      }
+      else {
+        console.error('Error in API request: ', response.statusText);
+      }
+    } 
+    catch (error) {
+      console.error("Failed to fetch data", error);
+    }
+  } 
   
-      setTimeout(() => {
-        setFlashingButtons((prev) => {
-          const newState = [...prev];
-          newState[index] = false;
-          return newState;
-        });
-      }, 200);
-    };
+  const handleArray = () => {
+    if (datas && datas.level >=4) {
+      for(let i=0; i<datas.level; i++){
+        flashButton(datas.createdList[i]);
+      }
+      setTimeout(() => {setFlashingButtons(Array(25).fill(false));},2000);
+    } 
+    else{
+      console.error("Data is either undefined or doesn't have enough elements");
+    }
+  }; 
+  const flashButton = (index: number) => {
+    setFlashingButtons((prev) => {
+      const newState = [...prev];
+      newState[index] = true;
+      return newState;
+    });
+  };
     return (
       <>
       <h3>Memory Matrix</h3>
@@ -63,11 +133,30 @@ function NRG() {
                 className={`grid-block ${flashingButtons[index] ? 'activated' : ''}`}
                 style={{ top: pos.top, left: pos.left, width: '100px', height: '100px' }}
                 onClick={() => {
-                    handleFlash(index);
-                  }}
+                    flashButton(index);
+                    if (datas) {
+                      const updatedData = {
+                          ...datas,
+                          expectedList: Array.isArray(datas.expectedList) ? [...datas.expectedList, index] : [index],
+                      };
+                      setData(updatedData);
+                      const dataString2 = updatedData.expectedList.join(', ');
+                      setDataString2(dataString2);
+                      if(updatedData.createdList.length==updatedData.expectedList.length){
+                        setFlashingButtons(Array(25).fill(false));
+                        postData(updatedData);
+            
+                      }
+                    }
+                 }}
           >
+          {datas && datas.createdList.includes(index)? datas.createdList.indexOf(index) + 1 : ''}
           </button>
         ))}
+        <div>{dataString1}</div>
+        <div>{dataString2}</div>
+        <button onClick={handleArray}></button>
+        <div >(index starts from zero)</div>
         </div>
         </div>
       </>

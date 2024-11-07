fe733b03fcaf7a48e9ac0c070cb6a1d8b02ead8a
gustason <gustas883@gmailcom>
2024-11-07 09:56:08 +0200

    Add tests

--- brainfreeze_new.Tests/Controllers/ScoreCntrollerTests.cs
+++ brainfreeze_new.Tests/Controllers/ScoreCntrollerTests.cs
@@ -33,5 +33,47 @@ namespace brainfreeze_new.Tests.Controllers
             // 3 matches * Normal multiplier (3) = 9
             Assert.Equal(9, response.Score);
         }
+
+        [Fact]
+        public void EvaluateScore_NoMatch()
+        {
+            var request = new ScoreEvaluationRequest
+            {
+                UserInput = new int[] { 4, 5, 6 },
+                Pattern = new int[] { 1, 2, 3 },
+                Difficulty = DifficultyLevel.Easy
+            };
+
+            var result = _controller.EvaluateScore(request) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+
+            var response = result.Value as ScoreResponse;
+            Assert.NotNull(response);
+            // No match => score is 0
+            Assert.Equal(0, response.Score);
+        }
+
+        [Fact]
+        public void EvaluateScore_NoMatchDifferentLength()
+        {
+            var request = new ScoreEvaluationRequest
+            {
+                UserInput = new int[] { 7 },
+                Pattern = new int[] { 1, 2, 3, 4 },
+                Difficulty = DifficultyLevel.Hard
+            };
+
+            var result = _controller.EvaluateScore(request) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+
+            var response = result.Value as ScoreResponse;
+            Assert.NotNull(response);
+            // No match => score is 0
+            Assert.Equal(0, response.Score);
+        }
     }
 }

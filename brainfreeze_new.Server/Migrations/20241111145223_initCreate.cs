e81a5e50e3550f4f2bcf8f67f4ce03ba56522e58
Nojus <nojus.cininas@mif.stud.vu.lt>
2024-11-11 17:37:31 +0200

    Added database models and migrations

---
+++ brainfreeze_new.Server/Migrations/20241111145223_initCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace brainfreeze_new.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class initCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "scoreboards",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    place = table.Column<int>(type: "int", nullable: false),
+                    username = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    simonScore = table.Column<int>(type: "int", nullable: false),
+                    cardflipScore = table.Column<int>(type: "int", nullable: false),
+                    nrgScore = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_scoreboards", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "scoreboards");
+        }
+    }
+}
---
+++ brainfreeze_new.Server/Models/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace brainfreeze_new.Server.Models
+{
+    public class Scoreboard
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int place { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public String username { get; set; }
+
+        public int simonScore { get; set; }
+
+        public int cardflipScore { get; set; }
+
+        public int nrgScore { get; set; }
+
+    }
+}
